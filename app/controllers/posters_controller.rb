07313e48f24226db1bf7ddb1a9d02d891ee36831
arfullight <tmimvhuang@gmail.com>
2016-02-25 19:53:51 +0800

    add user_type column and use ajax to create new poster

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -11,4 +11,62 @@ class PostersController < ApplicationController
 
   def show
   end
+
+  def new 
+
+  	@poster = Poster.new
+
+  end
+
+  def create
+  	
+  	data = params['data']
+  	
+  	data.each do |key, item|
+  		data[key] = to_b(item)
+  	end
+
+  	if data['location_white'] == '白色'
+  		data['location_white'] = true
+  	else
+  		data['location_white'] = false
+  	end
+  	
+  	
+  	@poster = Poster.new()
+  	@poster.use_avatar = data['use_avatar']
+  	@poster.name = data['name']
+  	@poster.description = data['description'] 	
+  	@poster.info_one = data['info_one']
+  	@poster.info_one_red = data['info_one_red']	
+  	@poster.info_two = data['info_two']
+  	@poster.info_two_red = data['info_two_red']
+  	@poster.info_three = data['info_three']
+  	@poster.info_three_red = data['info_three_red']
+  	@poster.location = data['location']
+  	@poster.location_white = data['location_white']
+
+
+  	@poster.save
+
+
+
+  	#@poster.save
+
+  	render :json => { success: true }
+
+  end
+
+  private
+
+  def to_b str
+  	if str == "true"
+  		return true
+  	elsif str == "false"
+  		return false
+  	else
+  		return str
+  	end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208040258) do
+ActiveRecord::Schema.define(version: 20160225115137) do
 
   create_table "posters", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160208040258) do
     t.string   "password_digest", limit: 255
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "user_type",       limit: 255
   end
 
   add_foreign_key "posters", "users"
