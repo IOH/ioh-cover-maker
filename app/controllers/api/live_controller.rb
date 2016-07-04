a9c32b424a244b1f47e36d9eba56b639be6a84c9
arfullight <tmimvhuang@gmail.com>
2016-07-04 22:04:02 +0800

    set api url to /api/live.json

--- app/controllers/api/live_controller.rb
+++ app/controllers/api/live_controller.rb
@@ -1,2 +1,9 @@
 class Api::LiveController < ApplicationController
+	def index
+		@lives = Live.all.select("name, time")
+
+		respond_to do |format|
+			format.json { render :json => @lives }
+		end
+	end
 end
