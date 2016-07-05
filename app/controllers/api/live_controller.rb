4c8f2d780a28db3a59b4950031d1a832dc837881
arfullight <tmimvhuang@gmail.com>
2016-07-05 14:10:39 +0800

    add sort_by

--- app/controllers/api/live_controller.rb
+++ app/controllers/api/live_controller.rb
@@ -1,9 +1,19 @@
 class Api::LiveController < ApplicationController
 	def index
+		sort_by = params[:sort_by]
+
+		case sort_by
+		when "school"
+			sort_by = "live_schools.name"
+		when "department"
+			sort_by = "live_departments.name"
+		end
+			
 		@lives = Live.joins(:live_department, :live_school)
 							   .select("lives.id, lives.name, lives.time, 
 							   					live_departments.name as department, 
 							   					live_schools.name as school")
+							   .order(sort_by)
 
 		respond_to do |format|
 			format.json { render :json => @lives }
